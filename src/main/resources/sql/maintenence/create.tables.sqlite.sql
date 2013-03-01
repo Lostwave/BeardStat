aaf23c301167d61c46cdf47eedc83dfb96477431
James <beardyjames@live.co.uk>
2013-03-01 22:52:13 +0000

    convertor failed, sqlite is annoying.

--- src/main/resources/sql/maintenence/create.tables.sqlite.sql
+++ src/main/resources/sql/maintenence/create.tables.sqlite.sql
@@ -1,6 +1,17 @@
-PRAGMA synchronous = OFF;
-PRAGMA journal_mode = MEMORY;
-BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS `${TBL_ENTITY}` (  `entityId` int(11) NOT NULL AUTO_INCREMENT,  `name` char(16) NOT NULL,  `type` enum('player','plugin','group') NOT NULL, PRIMARY KEY (`entityId`),  UNIQUE KEY `name` (`name`,`type`)) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-CREATE TABLE IF NOT EXISTS `${TBL_KEYSTORE}` (  `entityId` int(11) NOT NULL,  `domain` char(32) NOT NULL,  `world` char(32) NOT NULL,  `category` char(32) NOT NULL,  `statistic` char(32) NOT NULL,  `value` int(11) NOT NULL,  UNIQUE KEY `chkUni` (`entityId`,`domain`,`world`,`category`,`statistic`),  KEY `entityId` (`entityId`)) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-END TRANSACTION;
+CREATE TABLE IF NOT EXISTS `${TBL_ENTITY}` (  
+  `entityId` INTEGER PRIMARY KEY AUTOINCREMENT, 
+   `name` CHARACTER(16) NOT NULL,  
+   `type` CHARACTER(16) NOT NULL
+   );
+CREATE UNIQUE INDEX `name` (`name`,`type`);
+
+CREATE TABLE IF NOT EXISTS `${TBL_KEYSTORE}` (  
+  `entityId` INTEGER, 
+   `domain` CHARACTER(32) NOT NULL,  
+  `world` CHARACTER(32) NOT NULL,  
+  `category` CHARACTER(32) NOT NULL,  
+  `statistic` CHARACTER(32) NOT NULL,  
+  `value` INTEGER
+  );
+CREATE UNIQUE INDEX  `chkUni` (`entityId`,`domain`,`world`,`category`,`statistic`);
+CREATE INDEX `entityId` (`entityId`);
