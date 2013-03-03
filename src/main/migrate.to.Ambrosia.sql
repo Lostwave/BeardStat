25e95b9e7a6c3ff68b4b4ac783d5aa47acbc80b1
James <beardyjames@live.co.uk>
2013-03-03 00:47:33 +0000

    Add messages to migration script

--- src/main/migrate.to.Ambrosia.sql
+++ src/main/migrate.to.Ambrosia.sql
@@ -15,6 +15,8 @@
 -- 
 
 -- Create entity table
+SELECT "Creating entity table" as action;
+
 CREATE TABLE IF NOT EXISTS `stat_entity` ( 
   `entityId` int(11) NOT NULL AUTO_INCREMENT, 
   `name` char(16) NOT NULL,  
@@ -25,11 +27,14 @@ ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 
 -- Populate table with players
+SELECT "Populating entity table" as action;
 INSERT INTO `stat_entity` (`name`,`type`) SELECT `player`,"player" as `type` FROM `stats` GROUP BY `player`;
 
+SELECT "Indexing entity table" as action;
 ALTER TABLE `stat_entity` ADD UNIQUE KEY `name` (`name`,`type`);
 
 -- Create statkeystore
+SELECT "Creating keystore table" as action;
 CREATE TABLE IF NOT EXISTS `stat_keystore` (
   `entityId` int(11) NOT NULL,
   `domain` char(32) NOT NULL,
@@ -41,6 +46,7 @@ CREATE TABLE IF NOT EXISTS `stat_keystore` (
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Populate with data
+SELECT "Populating keystore table" as action;
 INSERT into `stat_keystore`
 SELECT  
 `entityId` , 
@@ -57,5 +63,7 @@ WHERE
 `type` =  'player';
 
 -- Re-initialise indexes
+SELECT "Indexing keystore table (WARNING: MAY TAKE A WHILE" as action;
  ALTER TABLE `stat_keystore` ADD UNIQUE KEY `chkUni` (`entityId`,`domain`,`world`,`category`,`statistic`);
  ALTER TABLE `stat_keystore` ADD KEY `entityId` (`entityId`);
+ SELECT "Finished!" as action;
