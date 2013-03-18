bbcde651ec42e09d7f31b9b92b14cb247f30ca52
James <beardyjames@live.co.uk>
2013-03-18 01:39:46 +0000

    Make /played work (hopefully)

--- src/main/java/com/tehbeard/BeardStat/commands/playedCommand.java
+++ src/main/java/com/tehbeard/BeardStat/commands/playedCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,75 +26,46 @@ public class playedCommand implements CommandExecutor {
 	}
 
 	public boolean onCommand(CommandSender sender, Command command, String cmdLabel, String[] args) {
-		long seconds = 0;
-		Player pp = null;
+		/*
+		 * ERRORS BLOCK
+		 * 
+		 * 
+		 * BeardStat.sendNoPermissionError(sender);
+		 * 
+		 * 
+		 * 
+		 */
 		
-		if (args.length == 0){
-			//check if we are a player, and use them instead
-			if(sender instanceof Player){
-				pp = (Player)sender;
-			}
-			else
-			{
-				sender.sendMessage(ChatColor.RED + LanguagePack.getMsg("command.error.noconsole.noargs"));
-				return true;
-			}
-		}
-
-
-		// they put a player name, try and find out who it is.
-		if(args.length == 1){
-			if(!BeardStat.hasPermission(sender, "command.played.other")){
-				BeardStat.sendNoPermissionError(sender);
-				return true;
-			}
-
-			pp = Bukkit.getPlayer(args[0]);
-			if(pp==null){
-				List<Player> ply = Bukkit.getServer().matchPlayer(args[0]);
-				if(ply.size()>1){
-					for(Player p:ply){
-						if(p.getName().equals(args[0])){
-							pp = p;
-							break;
-						}
-					}
-				} else if(ply.size()==1){
-					pp = ply.get(0);
-				}
-
-			}
-		}
-
-
+		long seconds = 0;
 		EntityStatBlob blob;
-
-		//no player match found, attempt to poll DB directly
-		if(pp==null){
-			blob = playerStatManager.findPlayerBlob(args[0]);//Try to find by exact name
+		
+		//select sender if they are a Player
+		OfflinePlayer selectedPlayer = (sender instanceof OfflinePlayer) ? (OfflinePlayer) sender : null;
+		
+		//Ok, try an exact player.dat match
+		if(args.length == 1 && BeardStat.hasPermission(sender, "command.played.other")){
+			selectedPlayer = Bukkit.getOfflinePlayer(args[0]).hasPlayedBefore() ? Bukkit.getOfflinePlayer(args[0]) : null;
 		}
-		else //poll using found player
-		{
-			blob = playerStatManager.findPlayerBlob(pp.getName());
-			seconds += BeardStat.self().getStatManager().getSessionTime(pp.getName());
-		}
-
-		if(blob != null && blob.getStat(BeardStat.DEFAULT_DOMAIN,BeardStat.GLOBAL_WORLD,"stats", "playedfor").getValue() != 0){
-			sender.sendMessage(ChatColor.GOLD + blob.getName());
-			seconds += blob.getStat(BeardStat.DEFAULT_DOMAIN,BeardStat.GLOBAL_WORLD,"stats","playedfor").getValue();
-			if(pp.isOnline()){
-				 seconds += BeardStat.self().getStatManager().getSessionTime(pp.getName());
-			}
-			sender.sendMessage(GetPlayedString(seconds));
+		
+		//failed to ascertain a player
+		if(selectedPlayer == null){
+			sender.sendMessage(ChatColor.RED + LanguagePack.getMsg("command.error.noconsole.noargs"));
 			return true;
-		}                   
-		else
-		{
+		}
+		
+		//TODO: async this
+		blob = playerStatManager.getPlayerBlob(selectedPlayer.getName());
+		if(blob == null){
 			sender.sendMessage(ChatColor.RED + LanguagePack.getMsg("command.error.noplayer",args[0]));
 			return true;
 		}
+		seconds = blob.getStat(BeardStat.DEFAULT_DOMAIN,BeardStat.GLOBAL_WORLD,"stats", "playedfor").getValue();
+		
+		seconds += playerStatManager.getSessionTime(selectedPlayer.getName());
+		sender.sendMessage(GetPlayedString(seconds));
+		
 
-
+		return true;
 	}
 
 	public static String GetPlayedString(long seconds){
