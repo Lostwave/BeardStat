d1ab98baf982c0ce0cfe8c95c30ead9ea74a461e
James <beardyjames@live.co.uk>
2013-03-18 16:38:31 +0000

    Add messages to migration script

--- src/main/resources/sql/maintenence/migration/migrate.2.sql
+++ src/main/resources/sql/maintenence/migration/migrate.2.sql
@@ -1,3 +1,5 @@
+#Migrating to yggdrasil format;
+#Creating entity table;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_entity` ( 
   `entityId` int(11) NOT NULL AUTO_INCREMENT, 
   `name` char(32) NOT NULL,  
@@ -5,10 +7,11 @@ CREATE TABLE IF NOT EXISTS `${PREFIX}_entity` (
   PRIMARY KEY (`entityId`)
   ) 
 ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
+#Populating entity table;
 INSERT INTO `${PREFIX}_entity` (`name`,`type`) SELECT `player`,"player" as `type` FROM `${OLD_TBL}` GROUP BY `player`;
+#Indexing entity key;
 ALTER TABLE `${PREFIX}_entity` ADD UNIQUE KEY `name` (`name`,`type`);
-
+#Creating domain table and initialising default domain;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_domain`(
   `domainId` int(11) NOT NULL AUTO_INCREMENT, 
   `domain` char(32) NOT NULL,  
@@ -16,30 +19,32 @@ CREATE TABLE IF NOT EXISTS `${PREFIX}_domain`(
 );
 INSERT INTO `${PREFIX}_domain` (`domain`) VALUES ("default");
 SET @domainId := (SELECT `domainId` from `${PREFIX}_domain` WHERE `domain` = "default");
-
+#Creating world table;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_world`(
   `worldId` int(11) NOT NULL AUTO_INCREMENT, 
   `world` char(32) NOT NULL,  
   PRIMARY KEY (`worldId`,`world`)
 );
+#Creating __imported__ world for stat migration;
 INSERT INTO `${PREFIX}_world` (`world`) VALUES ("__imported__");
 SET @worldId := (SELECT `worldId` from `${PREFIX}_world` WHERE `world` = "__imported__");
-
-
+#Creating category table;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_category`(
   `categoryId` int(11) NOT NULL AUTO_INCREMENT, 
   `category` char(32) NOT NULL,  
   PRIMARY KEY (`categoryId`,`category`)
 );
+#Populating category table;
 INSERT INTO `${PREFIX}_category` (`category`) SELECT DISTINCT(`category`) from stats;
-
+#Creating Statistic table;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_statistic`(
   `statisticId` int(11) NOT NULL AUTO_INCREMENT, 
   `statistic` char(32) NOT NULL,  
   PRIMARY KEY (`statisticId`,`statistic`)
 );
+#Populating statistic table;
 INSERT INTO `${PREFIX}_statistic` (`statistic`) SELECT DISTINCT(`stat`) from stats;
-
+#Creating value table
 CREATE TABLE IF NOT EXISTS `${PREFIX}_value` (
   `entityId`    int(11) NOT NULL,
   `domainId`    int(11) NOT NULL,
@@ -49,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `${PREFIX}_value` (
   `value`       int(11) NOT NULL
   ) 
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
+#Populating value table, WARNING: THIS WILL TAKE A LONG TIME WITH LARGE DATABASES;
 INSERT into `${PREFIX}_value`
 SELECT  
 `entityId` , 
@@ -70,7 +75,7 @@ WHERE
 `${PREFIX}_category`.`category` = `${OLD_TBL}`.`category` AND
 `${PREFIX}_statistic`.`statistic` = `stat`
 ;
-
+#Reindexing value table, this may take a while;
 ALTER TABLE `${PREFIX}_value` ADD UNIQUE KEY `chkUni` (`entityId`,`domainId`,`worldId`,`categoryId`,`statisticId`);
 ALTER TABLE `${PREFIX}_value` ADD KEY `entityId` (`entityId`);
-SELECT "Finished!" as action;
+#Migration to yggdrasil format complete;
