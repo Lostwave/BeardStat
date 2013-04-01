298dd663a383ecfa2c90617fe86f09de6f4a9624
James <beardyjames@live.co.uk>
2013-04-01 22:08:43 +0100

    Update create scripts to match migration scripts

--- src/main/resources/sql/maintenence/create.tables.sql
+++ src/main/resources/sql/maintenence/create.tables.sql
@@ -1,37 +1,35 @@
 CREATE TABLE IF NOT EXISTS `${PREFIX}_entity` ( 
   `entityId` int(11) NOT NULL AUTO_INCREMENT, 
-  `name` char(16) NOT NULL,  
+  `name` char(32) NOT NULL,  
   `type` enum('player','plugin','group') NOT NULL, 
   PRIMARY KEY (`entityId`),
-  UNIQUE KEY `name` (`name`,`type`)
-)
-ENGINE=InnoDB;
+  UNIQUE KEY (`name`,`type`)
+  ) 
+ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 CREATE TABLE IF NOT EXISTS `${PREFIX}_domain`(
   `domainId` int(11) NOT NULL AUTO_INCREMENT, 
   `domain` char(32) NOT NULL,  
-  PRIMARY KEY (`domainId`,`domain`)
-)
-ENGINE=InnoDB;
-INSERT INTO `${PREFIX}_domain` (`domain`) VALUES ("default");
+  PRIMARY KEY (`domainId`)
+  UNIQUE KEY (`domain`)
+);
 CREATE TABLE IF NOT EXISTS `${PREFIX}_world`(
   `worldId` int(11) NOT NULL AUTO_INCREMENT, 
   `world` char(32) NOT NULL,  
-  PRIMARY KEY (`worldId`,`world`)
-)
-ENGINE=InnoDB;
+  PRIMARY KEY (`worldId`),
+  UNIQUE KEY (`world`)
+);
 CREATE TABLE IF NOT EXISTS `${PREFIX}_category`(
   `categoryId` int(11) NOT NULL AUTO_INCREMENT, 
   `category` char(32) NOT NULL,  
-  PRIMARY KEY (`categoryId`,`category`)
-)
-ENGINE=InnoDB;
+  PRIMARY KEY (`categoryId`),
+  UNIQUE KEY (`category`)
+);
 CREATE TABLE IF NOT EXISTS `${PREFIX}_statistic`(
   `statisticId` int(11) NOT NULL AUTO_INCREMENT, 
   `statistic` char(32) NOT NULL,  
-  PRIMARY KEY (`statisticId`,`statistic`)
-)
-ENGINE=InnoDB;
-SELECT "Creating value table" as action;
+  PRIMARY KEY (`statisticId`),
+  UNIQUE KEY (`statistic`)
+);
 CREATE TABLE IF NOT EXISTS `${PREFIX}_value` (
   `entityId`    int(11) NOT NULL,
   `domainId`    int(11) NOT NULL,
@@ -39,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `${PREFIX}_value` (
   `categoryId`  int(11) NOT NULL,  
   `statisticId` int(11) NOT NULL,  
   `value`       int(11) NOT NULL,
-   UNIQUE KEY `chkUnique` (`entityId`,`domainId`,`worldId`,`categoryId`,`statisticId`),
-   INDEX `entityId` (`entityId`)
+  UNIQUE KEY (`entityId`, `domainId`, `worldId`, `categoryId`, `statisticId`)
   ) 
-ENGINE=InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ALTER TABLE `${PREFIX}_value` ADD KEY `entityId` (`entityId`);
