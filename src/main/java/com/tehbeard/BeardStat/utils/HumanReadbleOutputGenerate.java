e48cb20f35153034e5d41e8c4b3a3b61c3a9b612
James <beardyjames@live.co.uk>
2013-06-05 03:38:35 +0100

    Generate hooman readable names

--- src/main/java/com/tehbeard/BeardStat/utils/HumanReadbleOutputGenerate.java
+++ src/main/java/com/tehbeard/BeardStat/utils/HumanReadbleOutputGenerate.java
@@ -3,7 +3,11 @@ package com.tehbeard.BeardStat.utils;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.bukkit.Material;
 import org.bukkit.material.Dye;
@@ -17,6 +21,8 @@ import org.bukkit.material.Step;
 import org.bukkit.material.Tree;
 import org.bukkit.material.WoodenStep;
 import org.bukkit.material.Wool;
+
+import com.tehbeard.BeardStat.utils.MetaDataCapture.EntryInfo;
 
 public class HumanReadbleOutputGenerate {
 
@@ -35,6 +41,27 @@ public class HumanReadbleOutputGenerate {
         readers.put(Sandstone.class, "getType");
         readers.put(FlowerPot.class, "getContents");
 
+    }
+    
+    public static Map<String,String> generateHumanNames(){
+        Map<String,String> out = new TreeMap<String, String>();
+        for(Entry<Material, EntryInfo> e :MetaDataCapture.mats.entrySet()){
+            Material material  = e.getKey();
+            EntryInfo info     = e.getValue();
+            
+            Set<Integer> tags = new HashSet<Integer>();
+            for(int i =0;i<16;i++){
+                tags.add(info.getMetdataValue(i));
+            }
+            
+            for(int i : tags){
+                String bsid = material.toString().toLowerCase().replace("_", "") + "_" + i;
+                String humanName = getDataBasedName(material,(byte)(i&0xF));
+                out.put(bsid, humanName);
+            }
+            
+        }
+        return out;
     }
 
     public static String getDataBasedName(Material m, byte data) {
