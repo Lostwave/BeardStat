e614d9629fe2f65cb1234cf1679ba3797162cdf4
James <beardyjames@live.co.uk>
2013-06-13 17:44:21 +0100

    Remove unused function

--- src/web 2.0/api/sql.php
+++ src/web 2.0/api/sql.php
@@ -1,22 +1,8 @@
 <?php
 
-function getElementTable($element){
-  global $bs_db;
-  $e = $bs_db->real_escape_string($element);
-  echo "SELECT * FROM " . BS_DB_PREFIX . "_" . $e;
-  $bs_db->real_query("SELECT * FROM " . BS_DB_PREFIX . "_" . $e);
-  $res = $bs_db->store_result();
-  while($r = $res->fetch_assoc()){
-    $a[]=$r;
-  }
-  $res->free();
-  return $a;
-}
-
 function getLookup($element,$key){
  global $bs_db;
  $e = $bs_db->real_escape_string($element);
- echo "SELECT * FROM " . BS_DB_PREFIX . "_" . $e;
  $bs_db->real_query("SELECT * FROM " . BS_DB_PREFIX . "_" . $e);
  $res = $bs_db->store_result();
  while($r = $res->fetch_assoc()){
