db17a5a14c182368419d23fbaaa392dfa1c45599
James <beardyjames@live.co.uk>
2013-06-21 02:48:17 +0100

    Add category wrappers to migration script

--- src/main/resources/sql/maintenence/migration/migrate.3.sql
+++ src/main/resources/sql/maintenence/migration/migrate.3.sql
@@ -3,4 +3,5 @@
 ALTER TABLE  `${PREFIX}_statistic` ADD  `name` CHAR( 32 );
 ALTER TABLE  `${PREFIX}_statistic` ADD  `formatting` ENUM(  'none',  'timestamp',  'time' ) NOT NULL;
 ALTER TABLE  `${PREFIX}_entity`    ADD  `scoreboardhide` BOOLEAN NOT NULL;
+ALTER TABLE  `${PREFIX}_category`  ADD  `statwrapper` CHAR(64);
 #Done migrating
