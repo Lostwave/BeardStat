dec3aaf527d2fd4a30015fc0b7a1b96d33e6b296
James <beardyjames@live.co.uk>
2013-06-23 22:27:07 +0100

    Make type a string so we can store other types of entity

--- src/main/resources/sql/maintenence/migration/migrate.3.sql
+++ src/main/resources/sql/maintenence/migration/migrate.3.sql
@@ -4,4 +4,5 @@ ALTER TABLE  `${PREFIX}_statistic` ADD  `name` CHAR( 32 );
 ALTER TABLE  `${PREFIX}_statistic` ADD  `formatting` ENUM(  'none',  'timestamp',  'time' ) NOT NULL;
 ALTER TABLE  `${PREFIX}_entity`    ADD  `scoreboardhide` BOOLEAN NOT NULL;
 ALTER TABLE  `${PREFIX}_category`  ADD  `statwrapper` CHAR(64);
+ALTER TABLE  `${PREFIX}_entity` CHANGE  `type`  `type` CHAR( 16 );
 #Done migrating
