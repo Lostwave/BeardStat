4c0e15935f8e0463330a1d8d1c10da864b672850
James <beardyjames@live.co.uk>
2013-07-10 01:39:59 +0100

    Mockito fix

--- src/test/java/com/tehbeard/beardstat/listeners/TestBlockListener.java
+++ src/test/java/com/tehbeard/beardstat/listeners/TestBlockListener.java
@@ -4,8 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
