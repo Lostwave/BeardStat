c92a0981c7084acf0513e29729a08bfdf93da821
James <beardyjames@live.co.uk>
2013-09-09 01:08:36 +0100

    Begin work on scoreboards admin area.

---
+++ src/web 2.0/admin/getData.php
@@ -0,0 +1,12 @@
+<?php
+require 'api/api.php';
+//Provide data table for admin pages
+if(!isset($_chk)){
+$_chk = $_GET['id'];
+}
+$valid = $_chk == "domain" || $_chk == "world" || $_chk == "category" || $_chk == "statistic";
+if(!$valid){die()}
+echo json_encode(getLookup($_chk, $_chk));
+unset($_chk);
+?>
+
