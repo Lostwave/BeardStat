fd20f6069f393d98f0f30c8565810312b1a70bd2
James <beardyjames@live.co.uk>
2013-09-19 00:55:53 +0100

    Minor web fixes

--- src/web 2.0/admin/getData.php
+++ src/web 2.0/admin/getData.php
@@ -6,7 +6,7 @@ if(!isset($_chk)){
 $_chk = $_GET['id'];
 }
 $valid = $_chk == "domain" || $_chk == "world" || $_chk == "category" || $_chk == "statistic";
-if(!$valid){die()}
+if(!$valid){die();}
 echo json_encode(getLookup($_chk, $_chk));
 unset($_chk);
 ?>
