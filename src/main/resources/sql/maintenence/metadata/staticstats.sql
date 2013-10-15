03332dc30551a78a60b57ec948568266ed340718
James <beardyjames@live.co.uk>
2013-10-15 21:19:14 +0100

    I derp at sql

--- src/main/resources/sql/maintenence/metadata/staticstats.sql
+++ src/main/resources/sql/maintenence/metadata/staticstats.sql
@@ -1,6 +1,6 @@
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("play time","time","playedfor");
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("last login","timestamp","lastlogin");
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("first login","timestamp","firstlogin");
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("last logout","timestamp","lastlogout");
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("blocks placed","none",totalblockcreate");
-INSERT INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("blocks mined","none","totalblockdestroy");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("play time","time","playedfor");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("last login","timestamp","lastlogin");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("first login","timestamp","firstlogin");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("last logout","timestamp","lastlogout");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("blocks placed","none","totalblockcreate");
+INSERT IGNORE INTO ${PREFIX}_statistic (name,formatting,statistic) VALUES("blocks mined","none","totalblockdestroy");
