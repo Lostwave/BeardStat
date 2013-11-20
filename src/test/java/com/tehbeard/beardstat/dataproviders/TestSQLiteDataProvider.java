8e0e3f0e871b3cb02540e78010adbf3a3a9471d0
James <beardyjames@live.co.uk>
2013-11-20 19:03:26 +0000

    base SQLite test class, need to redo insert statements.
    for preloading.

--- src/test/java/com/tehbeard/beardstat/dataproviders/TestSQLiteDataProvider.java
+++ src/test/java/com/tehbeard/beardstat/dataproviders/TestSQLiteDataProvider.java
@@ -6,9 +6,13 @@ package com.tehbeard.beardstat.dataproviders;
 
 import com.tehbeard.beardstat.BeardStatRuntimeException;
 import com.tehbeard.beardstat.DatabaseConfiguration;
+import static com.tehbeard.beardstat.dataproviders.IStatDataProviderTest.instance;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
 import java.util.Properties;
+import org.junit.Assume;
+import org.junit.BeforeClass;
 
 /**
  *
@@ -16,22 +20,33 @@ import java.util.Properties;
  */
 
 public class TestSQLiteDataProvider extends IStatDataProviderTest  {
-    
-    public TestSQLiteDataProvider() throws IOException, SQLException{
+     
+    @BeforeClass
+    public static void setUpClass() throws IOException, SQLException {
+        InputStream is = TestMySQLDataProvider.class.getClassLoader().getResourceAsStream("mysql.properties");
+        if(is == null){
+            System.out.println("WARNING: MYSQL TEST NOT CONFIGURED, TEST SKIPPED.");
+        }
+        Assume.assumeTrue("MySQL test properties configured.", is != null);
         DatabaseConfiguration config = new DatabaseConfiguration(7);
         Properties properties = new Properties();
-        properties.load(getClass().getClassLoader().getResourceAsStream("mysql.properties"));
+        properties.load(is);
         new JavaPropertiesInjector(properties).inject(config);
         config.version = config.latestVersion;
         
-        
-        //instance = new MysqlStatDataProvider(new TestPlatform(), config);
-        try{
+        //System.out.println(config);
         instance = new SQLiteStatDataProvider(new TestPlatform(), ":memory:", config);
-        }catch(BeardStatRuntimeException e){
-            e.getCause().printStackTrace();
+       
+        String preloadStmt = ((SQLiteStatDataProvider)instance).readSQL("sql","preload",config.tablePrefix);
+        for(String s : preloadStmt.split("\\;")){
+            try{
+           ((SQLiteStatDataProvider)instance).conn.createStatement().execute(s);
+            }catch(SQLException e ){
+                e.printStackTrace();
+                
+                throw e;
+            }
         }
-        
     }
 
 }
