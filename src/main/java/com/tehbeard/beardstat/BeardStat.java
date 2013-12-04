5d787f7085ee6b88811cf70a362081da2bf791f2
James <beardyjames@live.co.uk>
2013-12-04 20:51:58 +0000

    Fix stat other permission node.

--- src/main/java/com/tehbeard/beardstat/BeardStat.java
+++ src/main/java/com/tehbeard/beardstat/BeardStat.java
@@ -53,7 +53,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 public class BeardStat extends JavaPlugin {
 
     public static final String PERM_COMMAND_PLAYED_OTHER = "stat.command.played.other";
-    public static final String PERM_COMMAND_STAT_OTHER = "command.stat.other";
+    public static final String PERM_COMMAND_STAT_OTHER = "stat.command.stat.other";
     // Default values for domain and world
     public static final String DEFAULT_DOMAIN = "default";
     public static final String GLOBAL_WORLD = "__global__";
