dc173d7b7e3125b69dd68e17542a4f1a3bfec1f5
James <beardyjames@live.co.uk>
2013-12-06 19:04:24 +0000

    Remove web 2.0 folder in prep for using BeardStatWebUI repo instead.

--- src/web 2.0/admin/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'session.php';
-unset($_SESSION['authed']);
-header("location","login.php");
-?>
--- src/web 2.0/templates/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<div class="footer">
-Powered by <a href="http://dev.bukkit.org/server-mods/BeardStat">BeardStat</a> ©2011-2013 <a href="http://tehbeard.com">Tehbeard</a>
-</div>
-</div>
-</body>
-</html>
